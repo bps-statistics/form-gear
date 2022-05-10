c8ca7ef22638367f63562d6d7356c0112539fed9
Ignatius Aditya Setyadi <ignatiusadityasetyadi@yahoo.com>
2022-05-10 07:36:29 +0700

    Update confirmation message dialog

--- src/components/NowInput.tsx
+++ src/components/NowInput.tsx
@@ -30,15 +30,15 @@ const NowInput: FormComponentBase = props => {
   const modalConfirmation = () => {
 		let titleModal = document.querySelector("#titleModalConfirmation");
 		let contentModal = document.querySelector("#contentModalConfirmation");
-		titleModal.innerHTML = props.component.titleModalConfirmation !== undefined ? props.component.titleModalConfirmation : 'Confirm Pick Time' ;
-		contentModal.innerHTML = props.component.contentModalConfirmation !== undefined ? props.component.contentModalConfirmation : 'Are you sure to pick the present time (now) ?';
+		titleModal.innerHTML = props.component.titleModalConfirmation !== undefined ? props.component.titleModalConfirmation : 'Confirmation' ;
+		contentModal.innerHTML = props.component.contentModalConfirmation !== undefined ? props.component.contentModalConfirmation : 'Are you certain to generate the current time?';
   }
 
   return (
     <div>
       <Show when={(showModal() == 1) }>
 				<div class="modal-confirmation fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-					<div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+					<div class="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
 						<div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
 						
 						<span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
@@ -60,7 +60,7 @@ const NowInput: FormComponentBase = props => {
 							</div>
 						</div>
 						<div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-							<button type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm" onClick={e => handleOnPick()}>Pick Time</button>
+							<button type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm" onClick={e => handleOnPick()}>Get Time</button>
 							<button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm" onClick={e => handleOnCancel()}>Cancel</button>
 						</div>
 						</div>
