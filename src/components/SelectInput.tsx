2de73fe8aa0814966a7b0ab40a04628a7d55c6f7
Ignatius Aditya Setyadi <ignatiusadityasetyadi@yahoo.com>
2022-05-28 09:44:52 +0700

    Update label view inline with required and hint

--- src/components/SelectInput.tsx
+++ src/components/SelectInput.tsx
@@ -357,15 +357,35 @@ const SelectInput: FormComponentBase = props => {
     }
     // console.log('valueny sekarang : ', props)
 
+    const [instruction, setInstruction] = createSignal(false);
+    const showInstruction = () => {
+      (instruction()) ? setInstruction(false) : setInstruction(true);
+    }
+
     const [enableRemark] = createSignal(config.formMode > 2 ? false : props.component.enableRemark !== undefined ? props.component.enableRemark : true);
 
     return (
         <div class="grid md:grid-cols-3 border-b border-gray-300/[.50] dark:border-gray-200/[.10] p-2">
-            <div class="font-light text-sm space-x-2 py-2.5 px-2">
+            <div class="font-light text-sm space-y-2 py-2.5 px-2">        
+                <div class="inline-flex space-x-2"> 
                 <div innerHTML={label()} />
                 <Show when={props.component.required}>
                     <span class="text-pink-600">*</span>
                 </Show>
+                <Show when={props.component.hint}>
+                    <button class="bg-transparent text-gray-300 rounded-full focus:outline-none h-4 w-4 hover:bg-gray-400 hover:text-white flex justify-center items-center"
+                    onClick={showInstruction}>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                    </button>
+                </Show>
+                </div>
+                <div class="flex mt-2">
+                <Show when={instruction()}>
+                    <div class="italic text-xs font-extralight text-zinc-400 " innerHTML={props.component.hint} />
+                </Show>
+                </div>
             </div>
             <div class="font-light text-sm space-x-2 py-2.5 px-2 md:col-span-2 grid grid-cols-12">
                 <Show when={isLoading()} fallback={
