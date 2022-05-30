4bbd9720bad665b042ad08ab103f833f0eacd875
D.Agung S <das.agung13@gmail.com>
2022-05-30 18:15:16 +0700

    fix bug input remark on formMode 3

--- src/components/CsvInput.tsx
+++ src/components/CsvInput.tsx
@@ -138,7 +138,7 @@ const CsvInput: FormComponentBase = props => {
         (instruction()) ? setInstruction(false) : setInstruction(true);
     }
 
-	const [enableRemark] = createSignal(config.formMode > 2 ? false : props.component.enableRemark !== undefined ? props.component.enableRemark : true );
+	const [enableRemark] = createSignal(props.component.enableRemark !== undefined ? props.component.enableRemark : true );
 
   return (
     <div>
