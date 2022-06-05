d166d519d17f808aa1b84a058aefa23a0e67a62f
Ignatius Aditya Setyadi <ignatiusadityasetyadi@yahoo.com>
2022-06-05 17:24:49 +0700

    Disable button on formMode > 1

--- src/components/ListTextInputRepeat.tsx
+++ src/components/ListTextInputRepeat.tsx
@@ -233,12 +233,16 @@ const ListTextInputRepeat: FormComponentBase = props => {
 											/>
 										</div>
 										<div class="col-span-2 flex justify-evenly p-1 space-x-1 ">
-											<button class="bg-teal-400 text-white p-2 rounded-full focus:outline-none hover:bg-teal-300" onClick={e => handleOnSave(Number(item.value))}>
+											<button class="bg-teal-400 text-white p-2 rounded-full focus:outline-none hover:bg-teal-300 disabled:bg-gray-200 dark:disabled:bg-gray-700 dark:disabled:text-gray-400"
+												disabled={disableInput()}
+												onClick={e => handleOnSave(Number(item.value))}>
 												<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
 													<path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
 												</svg>
 											</button>
-											<button class="bg-gray-500 text-white p-2 rounded-full focus:outline-none hover:bg-gray-400" onClick={e => handleOnCancel(Number(item.value))}>
+											<button class="bg-gray-500 text-white p-2 rounded-full focus:outline-none hover:bg-gray-400 disabled:bg-gray-200 dark:disabled:bg-gray-700 dark:disabled:text-gray-400"
+												disabled={disableInput()}
+												onClick={e => handleOnCancel(Number(item.value))}>
 												<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
 													<path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
 												</svg>
@@ -269,12 +273,16 @@ const ListTextInputRepeat: FormComponentBase = props => {
 											/>
 										</div>
 										<div class="col-span-2 flex justify-evenly p-1 space-x-1 ">
-											<button class="bg-orange-400 text-white p-2 rounded-full focus:outline-none hover:bg-orange-300" onClick={e => handleOnEdit(Number(item.value))}>
+											<button class="bg-orange-400 text-white p-2 rounded-full focus:outline-none hover:bg-orange-300 disabled:bg-gray-200 dark:disabled:bg-gray-700 dark:disabled:text-gray-400"
+												disabled={disableInput()}
+												onClick={e => handleOnEdit(Number(item.value))}>
 												<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
 													<path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
 												</svg>
 											</button>
-											<button class="bg-red-600 text-white p-2 rounded-full focus:outline-none hover:bg-red-500" onClick={e => handleOnDelete(Number(item.value))}>
+											<button class="bg-red-600 text-white p-2 rounded-full focus:outline-none hover:bg-red-500 disabled:bg-gray-200 dark:disabled:bg-gray-700 dark:disabled:text-gray-400"
+												disabled={disableInput()}
+												onClick={e => handleOnDelete(Number(item.value))}>
 												<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
 													<path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
 												</svg>
@@ -306,12 +314,16 @@ const ListTextInputRepeat: FormComponentBase = props => {
 								/>
 							</div>
 							<div class="col-span-2 flex justify-evenly p-1 space-x-1 ">
-								<button class="bg-teal-400 text-white p-2 rounded-full focus:outline-none hover:bg-teal-300" onClick={e => handleOnSave((getLastId() + 1))}>
+								<button class="bg-teal-400 text-white p-2 rounded-full focus:outline-none hover:bg-teal-300  disabled:bg-gray-200 dark:disabled:bg-gray-700 dark:disabled:text-gray-400"
+									disabled={disableInput()}
+									onClick={e => handleOnSave((getLastId() + 1))}>
 									<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
 										<path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
 									</svg>
 								</button>
-								<button class="bg-gray-500 text-white p-2 rounded-full focus:outline-none hover:bg-gray-400" onClick={e => handleOnCancel((getLastId() + 1))}>
+								<button class="bg-gray-500 text-white p-2 rounded-full focus:outline-none hover:bg-gray-400  disabled:bg-gray-200 dark:disabled:bg-gray-700 dark:disabled:text-gray-400"
+									disabled={disableInput()}
+									onClick={e => handleOnCancel((getLastId() + 1))}>
 									<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
 										<path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
 									</svg>
