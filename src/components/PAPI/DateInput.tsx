34e8c713de2b167731dd96d7f30e1e73405160c2
Muhammad Qadri <muhammad.qadri@bps.go.id>
2022-08-18 13:24:36 +0700

    Validate date input using dayjs in PAPI

--- src/components/PAPI/DateInput.tsx
+++ src/components/PAPI/DateInput.tsx
@@ -1,9 +1,12 @@
 import { createInputMask } from "@solid-primitives/input-mask"
-import { createMemo, createSignal, For, Match, Show, Switch } from "solid-js"
-import { ClientMode } from "../../constants"
+import dayjs from "dayjs"
+import CustomParseFormat from "dayjs/plugin/customParseFormat"
+import { createSignal } from "solid-js"
 import { handleInputFocus, handleInputKeyDown } from "../../events"
 import { FormComponentBase } from "../../FormType"
 import { InputContainer } from "./partials"
+
+dayjs.extend(CustomParseFormat)
 
 const DateInput: FormComponentBase = props => {
   const config = props.config
@@ -11,28 +14,7 @@ const DateInput: FormComponentBase = props => {
 
   let classInput = 'w-full rounded font-light px-4 py-2.5 text-sm text-gray-700 bg-white bg-clip-padding transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none disabled:bg-gray-200 dark:disabled:bg-gray-700 dark:disabled:text-gray-400';
 
-  const [instruction, setInstruction] = createSignal(false);
-  const showInstruction = () => {
-    (instruction()) ? setInstruction(false) : setInstruction(true);
-  }
-
-  const [disableClickRemark] = createSignal((config.formMode > 2 && props.comments == 0) ? true : false);
-  const [enableRemark] = createSignal((props.component.enableRemark !== undefined ? props.component.enableRemark : true) && config.clientMode != ClientMode.PAPI);
-
-  const changeFormat = (value: any, isReverse: boolean = false) => {
-    const splittedChar = isReverse ? "-" : "/"
-    const joinChar = isReverse ? "/" : "-"
-    const splitted = value.split(splittedChar)
-    splitted.reverse()
-    value = splitted.join(joinChar)
-    return value
-  }
-
-  let handleOnChange = (value: any) => {
-    value = changeFormat(value)
-    props.onValueChange(value)
-  }
-
+  const format = "DD/MM/YYYY"
   const maskingFormat = "99/99/9999"
   const formatMask = createInputMask(maskingFormat);
   let ref
@@ -44,29 +26,12 @@ const DateInput: FormComponentBase = props => {
     }
   };
 
-  let settedValue = (props.value) ? changeFormat(props.value, true) : ""
-
-
-  let today = new Date();
-  let dd = String(today.getDate());
-  let mm = String(today.getMonth() + 1); //January is 0!
-  let yyyy = String(today.getFullYear());
-  if (Number(dd) < 10) {
-    dd = '0' + dd
-  }
-  if (Number(mm) < 10) {
-    mm = '0' + mm
+  let handleOnChange = (value: any) => {
+    value = dayjs(value, format, true).format("YYYY-MM-DD")
+    props.onValueChange(value)
   }
 
-  let todayDate = yyyy + '-' + mm + '-' + dd;
-
-  let minDate: any, maxDate: any;
-  createMemo(() => {
-    if (props.component.rangeInput) {
-      minDate = (props.component.rangeInput[0].min !== undefined) ? (props.component.rangeInput[0].min === 'today') ? todayDate : props.component.rangeInput[0].min : '';
-      maxDate = (props.component.rangeInput[0].max !== undefined) ? (props.component.rangeInput[0].max === 'today') ? todayDate : props.component.rangeInput[0].max : '';
-    }
-  })
+  let settedValue = (props.value) ? dayjs(props.value).format(format) : ""
 
   return (
     <InputContainer component={props.component} classValidation={props.classValidation} validationMessage={props.validationMessage}>
